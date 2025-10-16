3927d935e7b0558fe76c9c07e90d312edbe1ef16
PKaur <parmindersarbjitkaur@gmail.com>
2025-10-16 03:11:11 -0700

    add architect state

--- agent/graph.py
+++ agent/graph.py
@@ -1,26 +1,38 @@
+from sys import implementation
+
+from langgraph.graph import StateGraph
 from dotenv import load_dotenv
 load_dotenv()
 from langchain.globals import set_verbose, set_debug
 from langchain_groq import ChatGroq
-from states import *
-from prompts import *
-from langgraph.graph import StateGraph
+from langgraph.constants import END
+from agent.prompts import *
+from agent.states import *
 llm = ChatGroq(model="openai/gpt-oss-120b")
 set_debug(True)
 set_verbose(True)
 def planner_agent(state:dict) -> dict:
     resp = llm.with_structured_output(Plan).invoke( planner_prompt(state["user_prompt"]))
-    return resp
+    return {"plan": resp }
 
+def architect_agent(state:dict) -> dict:
+    res = (llm.with_structured_output(ImplementationSteps).
+           invoke(architect_prompt(state["plan"])))
+    res.plan = state["plan"]
+    return { "implementation_steps" : res}
 
 user_prompt = "create a simple calculator web application"
 
 graph = StateGraph(dict)
 graph.add_node("planner", planner_agent)
+graph.add_node("architect", architect_agent)
+
+graph.add_edge("planner", "architect")
+
 graph.set_entry_point("planner")
+
 agent = graph.compile()
 result = agent.invoke({"user_prompt":user_prompt})
 
 
-resp = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
-print(resp)
+print(result)
--- agent/states.py
+++ agent/states.py
@@ -1,7 +1,7 @@
 
 
 from pydantic import BaseModel, Field
-
+from pydantic import ConfigDict
 
 class File(BaseModel):
     path: str = Field(description="")
@@ -11,6 +11,14 @@ class File(BaseModel):
 class Plan(BaseModel):
     name: str = Field(description= "")
     description: str = Field(description= "")
-    techstack: str = Field(description= "")
+    stack: str = Field(description= "")
     features: list[str] = Field(description= "")
     files: list[File] = Field(description= "")
+
+class TaskDetails(BaseModel):
+    filepath: str = Field(description= "")
+    description: str = Field(description= "")
+
+class ImplementationSteps(BaseModel):
+    tasks: list[TaskDetails] = Field(description= "")
+    model_config = ConfigDict(extra="allow")
